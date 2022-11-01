3f1854dfcc2679c0991f8403fe2ad8e9d4ea601a
SimonWanjir <simwanjir@gmail.com>
2022-10-31 18:52:24 -0700

    task 0 fix 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,6 +14,7 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 	x = 0;
+
 	while (b[x] != '\0')
 	{
 		if (b[x] != '0' && b[x] != '1')
@@ -21,6 +22,7 @@ unsigned int binary_to_uint(const char *b)
 			return (0);
 		}
 		count <<= 1;
+
 		if (b[x] == '1')
 		{
 			count ^= check;
